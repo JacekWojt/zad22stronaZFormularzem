56b27ebf7674f3970017d6906ed930275c8f1594
Jacek Wojtuszek <jacek.wojtuszek@gmail.com>
2023-04-21 18:54:06 +0200

    Stworzenie projektu

---
+++ src/main/java/pl/jw/stronazformularzem/StronaZFormularzemApplication.java
@@ -0,0 +1,13 @@
+package pl.jw.stronazformularzem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StronaZFormularzemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StronaZFormularzemApplication.class, args);
+    }
+
+}
